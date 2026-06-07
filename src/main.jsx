b5fa2c9a2da82c1ead0b361dd2932df7b7818b7b
Kishor73 <kishorrathod6203@gmail.com>
2026-06-07 14:57:16 +0530

    Added Vercel Analytics and Speed Insights

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,25 @@
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import './index.css'
+// import App from './App.jsx'
+
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/react'
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <Analytics />
+    <SpeedInsights />
   </StrictMode>,
 )
